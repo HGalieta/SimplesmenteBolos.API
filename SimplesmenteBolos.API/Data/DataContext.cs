ca4e47de7645d6bd0546cf3f04c5eb07272a724f
Henrique Galieta <hgalieta@gmail.com>
2023-03-28 20:22:07 -0300

    feat: add datacontext and db connection configuration

---
+++ SimplesmenteBolos.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimplesmenteBolos.API.Models;
+using System.Collections.Generic;
+
+namespace SimplesmenteBolos.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Bolo> Bolos { get; set; }
+    }
+}
