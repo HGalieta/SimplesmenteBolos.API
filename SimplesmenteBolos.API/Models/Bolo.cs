c6e33a32f84b5f1d61681468d3b237e538db60d2
Henrique Galieta <hgalieta@gmail.com>
2023-03-28 20:18:42 -0300

    feat: add bolo model

---
+++ SimplesmenteBolos.API/Models/Bolo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimplesmenteBolos.API.Models
+{
+    public class Bolo
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string FileName { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
