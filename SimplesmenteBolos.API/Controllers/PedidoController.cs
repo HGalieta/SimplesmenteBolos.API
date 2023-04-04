47b25f26d6f5b8692923dd70903e70f12b51045c
Henrique Galieta <hgalieta@gmail.com>
2023-04-03 21:37:37 -0300

    feat: add pedido model and controller

---
+++ SimplesmenteBolos.API/Controllers/PedidoController.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SimplesmenteBolos.API.Data;
+using SimplesmenteBolos.API.Models;
+
+namespace SimplesmenteBolos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PedidoController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PedidoController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetPedidos()
+        {
+            List<Pedido> pedidos = _context.Pedidos.ToList();
+
+            if (pedidos != null)
+            {
+                return Ok(pedidos);
+            }
+
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPedidoById(int id)
+        {
+            Pedido pedido = _context.Pedidos.FirstOrDefault(v => v.Id == id);
+
+            if (pedido != null)
+            {
+                return Ok(pedido);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult PostPedido(Pedido pedido)
+        {
+            _context.Pedidos.Add(pedido);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetPedidoById), new { Id = pedido.Id }, pedido);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePedido(int id)
+        {
+            Pedido pedido = _context.Pedidos.FirstOrDefault(v => v.Id == id);
+
+            if (pedido != null)
+            {
+                _context.Pedidos.Remove(pedido);
+                _context.SaveChanges();
+                return Ok(pedido);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ SimplesmenteBolos.API/Models/Pedido.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace SimplesmenteBolos.API.Models
+{
+    public class Pedido
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public List<ItemPedido> ItensPedido { get; set; } = new List<ItemPedido>();
+    }
+}
