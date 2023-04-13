8c4390f1c0235efb573d0c27591c5f06d8bdc7b6
Henrique Galieta <hgalieta@gmail.com>
2023-04-13 11:23:44 -0300

    feat: add serialization to entity properties

--- SimplesmenteBolos.API/Data/DataContext.cs
+++ SimplesmenteBolos.API/Data/DataContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using SimplesmenteBolos.API.Models;
-using System.Collections.Generic;
+using System.Text.Json;
 
 namespace SimplesmenteBolos.API.Data
 {
@@ -8,14 +9,25 @@ namespace SimplesmenteBolos.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-        protected override void OnModelCreating(ModelBuilder builder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            builder.Entity<Pedido>()
-                .HasMany(p => p.ItensPedido)
-                .WithOne(i => i.Pedido);
+            modelBuilder.Entity<Pedido>()
+                .Property(e => e.Cliente)
+                .HasConversion(
+                    v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+                    v => JsonSerializer.Deserialize<Cliente>(v, (JsonSerializerOptions)null));
+
+            modelBuilder.Entity<Pedido>()
+                .Property(p => p.ItensPedido)
+                .HasConversion(
+                    v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+                    v => JsonSerializer.Deserialize<List<ItemPedido>>(v, (JsonSerializerOptions)null),
+                    new ValueComparer<List<ItemPedido>>(
+                        (c1, c2) => c1.SequenceEqual(c2),
+                        c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                        c => (List<ItemPedido>)c.ToList()));
         }
 
-        public DbSet<ItemPedido> Itens { get; set; }
         public DbSet<Pedido> Pedidos { get; set; }
     }
 }
